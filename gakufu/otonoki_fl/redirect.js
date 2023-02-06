70ba03e755ddb70766899df5dffa4d53ccdd308b
hiroki <yama.ri0023ef@gmail.com>
2023-02-07 02:31:56 +0900

    add : trb and fl page

---
+++ gakufu/otonoki_fl/redirect.js
@@ -0,0 +1,10 @@
+'user strict';
+
+var ref = document.referrer;
+
+if (ref.match(/index/) != null) {
+    // index.htmlから来た場合
+} else {
+    // index.html以外から来た場合
+    location.href = "./index.html";
+}
